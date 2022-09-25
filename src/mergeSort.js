87c2b745bef46beab796b6eca54f0e9d680632c4
pixcelo <bodomu@gmail.com>
2022-09-25 14:49:38 +0900

    mergeSort

---
+++ src/mergeSort.js
@@ -0,0 +1,33 @@
+// Merge sort
+function mergeSort(unsortedArray) {
+    if (unsortedArray.length <= 1) {
+      return unsortedArray;
+    }
+    const middle = Math.floor(unsortedArray.length / 2);
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+  }
+
+  function merge(left, right) {
+    let resultArray = [],
+      leftIndex = 0,
+      rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        resultArray.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        resultArray.push(right[rightIndex]);
+        rightIndex++;
+      }
+    }
+
+    return resultArray
+      .concat(left.slice(leftIndex))
+      .concat(right.slice(rightIndex));
+  }
+
+  console.log(mergeSort([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])); // [1, 2, 3, 4, 7, 8, 9, 10, 14, 16]
